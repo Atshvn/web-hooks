212e9877c16da3f37c51ff23948078df2bcc835f
Atshvn <tuananh.0706999@gmail.com>
2023-07-12 15:57:11 +0700

    hihi

--- index.js
+++ index.js
@@ -5,14 +5,19 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-
+let a = "Hello, world";
+const handleData = (req, res) => {
+  console.log(req.body);
+  a = req.body;
+};
 app.post("/webhook", (req, res) => {
   console.log("Received webhook:", req.body);
   res.send(req.body);
+  handleData(req, res);
   res.sendStatus(200);
 });
 app.get("/", (req, res) => {
-  res.send("Hello, world!");
+  res.send(a);
 });
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
